a10bb11c9dded79d17a0346dec2bcc43431f7456
Anton Heestand <anton.heestand@gmail.com>
2024-12-25 10:10:52 +0900

    Initial Commit

---
+++ KeyboardLingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyboardLingo
+//
+//  Created by Anton on 2024-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyboardLingo/KeyboardLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardLingoApp.swift
+//  KeyboardLingo
+//
+//  Created by Anton on 2024-12-25.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
